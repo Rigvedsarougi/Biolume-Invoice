e0852f71b133c9bace886dc62cb807cca6690134
Rigved Sarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2025-02-25 11:47:57 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+COMPANY_NAME = "KS Agencies"
+COMPANY_ADDRESS = """61A/42, Karunanidhi Street, Nehru Nagar,
+West Velachery, Chennai - 600042.
+GSTIN/UIN: 33AAGFK1394P1ZX
+State Name : Tamil Nadu, Code : 33
+"""
+
+COMPANY_LOGO = 'assets/logo.png'
+PHOTO_LOGO = 'assets/photo.png'
+
+BANK_DETAILS = """
+For Rtgs / KS Agencies
+Kotak Mahindra Bank
+Velachery branch
+Ac No 0012490288
+IFSC code KKBK0000473
+Mobile - 9444454461 / GPay / PhonePe / Niyas
+"""
